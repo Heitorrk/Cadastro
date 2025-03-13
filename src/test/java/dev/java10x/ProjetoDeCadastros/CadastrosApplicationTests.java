ed60f81f847ceac18caad7972cb332df3d4f5610
Heitorrk <heitor.mamede@hotmail.com>
2025-03-13 16:46:24 -0300

    first commit

---
+++ src/test/java/dev/java10x/ProjetoDeCadastros/CadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.ProjetoDeCadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
